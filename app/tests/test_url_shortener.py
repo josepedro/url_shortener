909fb35274ae1b59709b7783f5b33b24afbe7e33
José Pedro <1jpsneto@gmail.com>
2017-08-15 01:37:35 -0300

    Test example done

--- app/tests/test_url_shortener.py
+++ app/tests/test_url_shortener.py
@@ -7,16 +7,14 @@ import app.url_shortener.url_shortener
 class UrlShortnerTestCase(unittest.TestCase):
 
     def setUp(self):
-        print "rodando a parada"
         app.url_shortener.url_shortener.app.testing = True
 
     def tearDown(self):
         pass
 
     def test_first(self):
-    	print "ollaa"
+    	assert 2 + 3 == 5, "Houston we've got a problem" 
 
 
 if __name__ == '__main__':
-    #print os.path.dirname(__file__)
 	unittest.main()
