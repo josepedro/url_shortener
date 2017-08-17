92804d371e594bec831cee3220c3207e037fa737
José Pedro <1jpsneto@gmail.com>
2017-08-17 00:42:53 -0300

    Install script done

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
+#!/usr/bin/env 
+
 sudo apt-get update && 
 sudo apt-get -y install python-dev && 
 sudo apt-get -y install python-pip && 
 sudo pip install virtualenvwrapper && 
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc && 
-source ~/.bashrc && 
+#echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc &&
+#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc && 
+#source ~/.bashrc && 
+source /usr/local/bin/virtualenvwrapper.sh
 sudo pip install IPython==5.0 && 
 sudo pip install -U pip setuptools && 
-sudo apt-get -y install git && 
-git clone https://github.com/josepedro/url_shortener.git && 
-cd url_shortener && 
+#sudo apt-get -y install git && 
 mkvirtualenv url_shortener_env && 
-pip install -e . && 
+pip install -e . &&
+touch test.db && 
 python prepare_database.py && 
 python setup.py test && 
 echo "==== Everything ok! Type sh start.sh to run application. ===="
