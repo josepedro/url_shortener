7575834ce5682f28cb290230a6b538a8eb92c6e3
José Pedro <1jpsneto@gmail.com>
2017-08-17 03:10:18 -0300

    Fiz installation script

--- install.sh
+++ install.sh
@@ -5,15 +5,13 @@ sudo apt-get -y install python-dev &&
 sudo apt-get -y install python-pip && 
 sudo pip install virtualenvwrapper && 
 #echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc &&
-#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc && 
-#source ~/.bashrc && 
-source /usr/local/bin/virtualenvwrapper.sh
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc && 
+source ~/.bashrc && 
 sudo pip install IPython==5.0 && 
 sudo pip install -U pip setuptools && 
 #sudo apt-get -y install git && 
-mkvirtualenv url_shortener_env && 
-pip install -e . &&
-touch test.db && 
+#mkvirtualenv url_shortener_env && 
+#pip install -e . &&
 python prepare_database.py && 
 python setup.py test && 
 echo "==== Everything ok! Type sh start.sh to run application. ===="
